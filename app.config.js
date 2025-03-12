572093c230a4fd70d2ecc793b7911ef590cbc564
Christopher David <chris@openagents.com>
2025-03-12 01:25:05 -0500

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,4 +1,47 @@
 module.exports = {
-  // This empty config file is needed to fix the EXConstants build error
-  // See: https://github.com/expo/expo/issues/15809
+  name: "onyx2",
+  slug: "onyx2",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.openagents.onyx2"
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    },
+    package: "com.openagents.onyx2"
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png"
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        "image": "./assets/images/splash-icon.png",
+        "imageWidth": 200,
+        "resizeMode": "contain",
+        "backgroundColor": "#ffffff"
+      }
+    ],
+    [
+      "@breeztech/react-native-breez-sdk",
+      {
+        "permissions": ["INTERNET", "ACCESS_NETWORK_STATE"]
+      }
+    ]
+  ],
+  experiments: {
+    typedRoutes: true
+  }
 };
